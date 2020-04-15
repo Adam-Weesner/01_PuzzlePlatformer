ff31d8821f22fcbfabeb8b3aa4b0891dd21a4788
Klaviam <klaviam@gmail.com>
2020-04-15 10:02:44 -0700

    Client can join IP

--- Source/PuzzlePlatformer/Instance_PuzzlePlatformer.cpp
+++ Source/PuzzlePlatformer/Instance_PuzzlePlatformer.cpp
@@ -1,7 +1,8 @@
 // Written by Adam Weesner @2020
 #include "Instance_PuzzlePlatformer.h"
 #include "Engine/Engine.h"
-#include "Misc/Paths.h"
+#include "Misc/Paths.h" 	
+#include "GameFramework/PlayerController.h"
 
 UInstance_PuzzlePlatformer::UInstance_PuzzlePlatformer(const FObjectInitializer& ObjectInitializer)
 {
@@ -16,7 +17,7 @@ void UInstance_PuzzlePlatformer::Init()
 void UInstance_PuzzlePlatformer::Host()
 {
 	UEngine* Engine = GetEngine();
-	if (!Engine) return;
+	if (!ensure(Engine)) return;
 
 	FString output = "Hosting";
 	Engine->AddOnScreenDebugMessage(0, 5.0f, FColor::Yellow, output);
@@ -31,6 +32,11 @@ void UInstance_PuzzlePlatformer::Join(const FString address)
 	FString output = "Joining: " + address;
 
 	Engine->AddOnScreenDebugMessage(0, 5.0f, FColor::Yellow, output);
+
+	APlayerController* PlayerController = GetFirstLocalPlayerController();
+	if (!ensure(PlayerController)) return;
+
+	PlayerController->ClientTravel(address, ETravelType::TRAVEL_Absolute);
 }
 
 void UInstance_PuzzlePlatformer::NextMap()
